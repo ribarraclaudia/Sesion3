ac5ac3b4b6b94fe2345db2ffdf79ada355f88a1c
ibarraclaudia <ribarra.claudia@gmail.com>
2023-06-17 10:18:05 -0600

    Creacion activity

---
+++ app/src/main/java/com/pruebacurso1/sesion3miprimeractivity/Main/Main.java
@@ -0,0 +1,5 @@
+package com.pruebacurso1.sesion3miprimeractivity.Main;
+import android.app.Activity;
+
+public class Main {
+}
---
+++ app/src/main/java/com/pruebacurso1/sesion3miprimeractivity/Main/MainActivityKotlin.kt
@@ -0,0 +1,7 @@
+package com.pruebacurso1.sesion3miprimeractivity.Main
+
+import android.app.Activity
+
+class MainActivityKotlin: Activity() {
+
+}
